536a490eb8a1ab553ebc91d15b01353df14d9837
Valinor\Feanor <jaime.trollhammaren@gmail.com>
2017-07-29 16:00:12 +0200

    Renamed Main to PowerCharge and changed source compatibility to Java 8

--- desktop/src/com/alienshots/ludum/desktop/DesktopLauncher.java
+++ desktop/src/com/alienshots/ludum/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
 package com.alienshots.ludum.desktop;
 
+import com.alienshots.ludum.PowerCharge;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.alienshots.ludum.Main;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Main(), config);
+		new LwjglApplication(new PowerCharge(), config);
 	}
 }
--- html/src/com/alienshots/ludum/client/HtmlLauncher.java
+++ html/src/com/alienshots/ludum/client/HtmlLauncher.java
@@ -3,7 +3,7 @@ package com.alienshots.ludum.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import com.alienshots.ludum.Main;
+import com.alienshots.ludum.PowerCharge;
 
 public class HtmlLauncher extends GwtApplication {
 
@@ -14,6 +14,6 @@ public class HtmlLauncher extends GwtApplication {
 
         @Override
         public ApplicationListener createApplicationListener () {
-                return new Main();
+                return new PowerCharge();
         }
 }
