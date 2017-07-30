e252a68c3c717031dc916919ab42460e63df2b5b
ferminho <ferminho@gmail.com>
2017-07-30 18:35:10 +0200

    All player positions

--- core/src/com/alienshots/ludum/asset/texture/GameScreenAtlas.java
+++ core/src/com/alienshots/ludum/asset/texture/GameScreenAtlas.java
@@ -41,6 +41,7 @@ public class GameScreenAtlas {
 
     private void initPlayerRegions() {
         String baseName = PlayerComponent.class.getName();
+        // first floor
         textureAtlas.addRegion(baseName + "1_1_DOWN", sourceImage, 192, 571, 30, 37);
         textureAtlas.addRegion(baseName + "1_2_DOWN", sourceImage, 329, 603, 26, 37);
         textureAtlas.addRegion(baseName + "1_3_DOWN", sourceImage, 455, 603, 26, 37);
@@ -56,9 +57,54 @@ public class GameScreenAtlas {
         textureAtlas.addRegion(baseName + "1_6_UP", sourceImage, 831, 547, 28, 30);
         textureAtlas.addRegion(baseName + "1_7_UP", sourceImage, 957, 538, 28, 30);
         textureAtlas.addRegion(baseName + "1_8_UP", sourceImage, 1052, 538, 25, 36);
-
-
-
+        // second floor
+        textureAtlas.addRegion(baseName + "2_1_DOWN", sourceImage, 192, 571, 25, 36);
+        textureAtlas.addRegion(baseName + "2_2_DOWN", sourceImage, 275, 483, 26, 37);
+        textureAtlas.addRegion(baseName + "2_3_DOWN", sourceImage, 401, 483, 26, 37);
+        textureAtlas.addRegion(baseName + "2_4_DOWN", sourceImage, 527, 483, 26, 37);
+        textureAtlas.addRegion(baseName + "2_5_DOWN", sourceImage, 653, 483, 26, 37);
+        textureAtlas.addRegion(baseName + "2_6_DOWN", sourceImage, 779, 483, 26, 37);
+        textureAtlas.addRegion(baseName + "2_7_DOWN", sourceImage, 908, 483, 26, 37);
+        textureAtlas.addRegion(baseName + "2_8_DOWN", sourceImage, 1034, 483, 26, 37);
+        textureAtlas.addRegion(baseName + "2_1_UP", sourceImage, 206, 418, 25, 36);
+        textureAtlas.addRegion(baseName + "2_2_UP", sourceImage, 274, 427, 28, 30);
+        textureAtlas.addRegion(baseName + "2_3_UP", sourceImage, 400, 426, 28, 30);
+        textureAtlas.addRegion(baseName + "2_4_UP", sourceImage, 526, 432, 28, 30);
+        textureAtlas.addRegion(baseName + "2_5_UP", sourceImage, 652, 431, 28, 30);
+        textureAtlas.addRegion(baseName + "2_6_UP", sourceImage, 778, 426, 28, 30);
+        textureAtlas.addRegion(baseName + "2_7_UP", sourceImage, 907, 427, 28, 30);
+        textureAtlas.addRegion(baseName + "2_8_UP", sourceImage, 1033, 434, 28, 30);
+        // third floor
+        textureAtlas.addRegion(baseName + "3_1_DOWN", sourceImage, 227, 363, 26, 37);
+        textureAtlas.addRegion(baseName + "3_2_DOWN", sourceImage, 351, 363, 26, 37);
+        textureAtlas.addRegion(baseName + "3_3_DOWN", sourceImage, 477, 363, 26, 37);
+        textureAtlas.addRegion(baseName + "3_4_DOWN", sourceImage, 603, 363, 26, 37);
+        textureAtlas.addRegion(baseName + "3_5_DOWN", sourceImage, 729, 363, 26, 37);
+        textureAtlas.addRegion(baseName + "3_6_DOWN", sourceImage, 855, 363, 26, 37);
+        textureAtlas.addRegion(baseName + "3_7_DOWN", sourceImage, 955, 354, 25, 36);
+        textureAtlas.addRegion(baseName + "3_8_DOWN", sourceImage, 1044, 363, 26, 37);
+        textureAtlas.addRegion(baseName + "3_1_UP", sourceImage, 274, 427, 28, 30);
+        textureAtlas.addRegion(baseName + "3_2_UP", sourceImage, 350, 307, 28, 30);
+        textureAtlas.addRegion(baseName + "3_3_UP", sourceImage, 476, 309, 28, 30);
+        textureAtlas.addRegion(baseName + "3_4_UP", sourceImage, 602, 312, 28, 30);
+        textureAtlas.addRegion(baseName + "3_5_UP", sourceImage, 728, 309, 28, 30);
+        textureAtlas.addRegion(baseName + "3_6_UP", sourceImage, 854, 306, 28, 30);
+        textureAtlas.addRegion(baseName + "3_7_UP", sourceImage, 956, 298, 25, 36);
+        textureAtlas.addRegion(baseName + "3_8_UP", sourceImage, 1043, 309, 28, 30);
+        // fourth floor
+        textureAtlas.addRegion(baseName + "4_1_DOWN", sourceImage, 266, 229, 30, 37);
+        textureAtlas.addRegion(baseName + "4_2_DOWN", sourceImage, 329, 244, 26, 37);
+        textureAtlas.addRegion(baseName + "4_3_DOWN", sourceImage, 455, 244, 26, 37);
+        textureAtlas.addRegion(baseName + "4_4_DOWN", sourceImage, 581, 244, 26, 37);
+        textureAtlas.addRegion(baseName + "4_5_DOWN", sourceImage, 707, 244, 26, 37);
+        textureAtlas.addRegion(baseName + "4_6_DOWN", sourceImage, 832, 244, 26, 37);
+        textureAtlas.addRegion(baseName + "4_7_DOWN", sourceImage, 956, 244, 26, 37);
+        textureAtlas.addRegion(baseName + "4_2_UP", sourceImage, 328, 179, 28, 30);
+        textureAtlas.addRegion(baseName + "4_3_UP", sourceImage, 454, 186, 28, 30);
+        textureAtlas.addRegion(baseName + "4_4_UP", sourceImage, 580, 188, 28, 30);
+        textureAtlas.addRegion(baseName + "4_5_UP", sourceImage, 706, 185, 28, 30);
+        textureAtlas.addRegion(baseName + "4_6_UP", sourceImage, 831, 183, 28, 30);
+        textureAtlas.addRegion(baseName + "4_7_UP", sourceImage, 956, 177, 30, 37);
     }
 
     private void initSawRegions() {
