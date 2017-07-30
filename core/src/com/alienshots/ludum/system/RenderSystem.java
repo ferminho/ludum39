8ce6cd9b8dbf755cc6567081be8b9632e39ab2e5
ferminho <ferminho@gmail.com>
2017-07-30 17:21:28 +0200

    Painting background with camera, first approach

--- core/src/com/alienshots/ludum/system/RenderSystem.java
+++ core/src/com/alienshots/ludum/system/RenderSystem.java
@@ -6,9 +6,14 @@ import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +21,9 @@ import java.util.List;
 public class RenderSystem extends IteratingSystem {
 
     private final Batch batch;
+    private final OrthographicCamera camera;
     private final Texture backgroundTexture;
+
     private final List<Entity> entities;
     private final ComponentMapper<PositionComponent> positionMapper;
     private final ComponentMapper<DisplayComponent> displayMapper;
@@ -25,6 +32,8 @@ public class RenderSystem extends IteratingSystem {
         super(Family.all(PositionComponent.class, DisplayComponent.class).get());
 
         this.batch = new SpriteBatch();
+        this.camera = new OrthographicCamera(1280f, 720f);
+        camera.position.set(new Vector3(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0));
         this.backgroundTexture = new Texture("background.png");
         this.entities = new ArrayList<>();
         this.positionMapper = ComponentMapper.getFor(PositionComponent.class);
@@ -39,6 +48,8 @@ public class RenderSystem extends IteratingSystem {
     @Override
     public void update(float deltaTime) {
         super.update(deltaTime);
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
 
         batch.begin();
         batch.draw(backgroundTexture, 0, 0);
