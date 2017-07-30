4a973ad47e4ff6a09d4f6211f58ba906944bf608
Valinor\Feanor <jaime.trollhammaren@gmail.com>
2017-07-30 16:20:24 +0200

    Added third coordinate (up/down) to positions

--- core/src/com/alienshots/ludum/asset/texture/GameScreenAtlas.java
+++ core/src/com/alienshots/ludum/asset/texture/GameScreenAtlas.java
@@ -22,7 +22,7 @@ public class GameScreenAtlas {
     private TextureAtlas textureAtlas;
 
     public GameScreenAtlas() {
-        sourceImage = new Texture("background.png");
+        sourceImage = new Texture("sprites.png");
         textureAtlas = new TextureAtlas();
 
         initNameTemplateLookup();
@@ -30,8 +30,8 @@ public class GameScreenAtlas {
     }
 
     private void initNameTemplateLookup() {
-        regionNameTemplateLookup.put(PlayerComponent.class, PlayerComponent.class.getName() + "%s_%s");
-        regionNameTemplateLookup.put(SawComponent.class, SawComponent.class.getName() + "%s_%s");
+        regionNameTemplateLookup.put(PlayerComponent.class, PlayerComponent.class.getName() + "%s_%s_%s");
+        regionNameTemplateLookup.put(SawComponent.class, SawComponent.class.getName() + "%s_%s_%s");
     }
 
     private void initAtlasRegions() {
@@ -41,23 +41,24 @@ public class GameScreenAtlas {
 
     private void initPlayerRegions() {
         String baseName = PlayerComponent.class.getName();
-        textureAtlas.addRegion(baseName + "1_1", sourceImage, 0, 0, 30, 30);
-        textureAtlas.addRegion(baseName + "1_2", sourceImage, 50, 50, 30, 30);
+        textureAtlas.addRegion(baseName + "1_1_DOWN", sourceImage, 0, 0, 30, 30);
+        textureAtlas.addRegion(baseName + "1_2_DOWN", sourceImage, 50, 50, 30, 30);
     }
 
     private void initSawRegions() {
         String baseName = SawComponent.class.getName();
-        textureAtlas.addRegion(baseName + "1_1", sourceImage, 100, 100, 30, 30);
-        textureAtlas.addRegion(baseName + "1_2", sourceImage, 150, 150, 30, 30);
-        textureAtlas.addRegion(baseName + "1_3", sourceImage, 200, 200, 30, 30);
-        textureAtlas.addRegion(baseName + "1_4", sourceImage, 250, 250, 30, 30);
-        textureAtlas.addRegion(baseName + "1_5", sourceImage, 300, 300, 30, 30);
-        textureAtlas.addRegion(baseName + "1_6", sourceImage, 350, 350, 30, 30);
-        textureAtlas.addRegion(baseName + "1_7", sourceImage, 400, 400, 30, 30);
+        textureAtlas.addRegion(baseName + "1_1_DOWN", sourceImage, 100, 100, 30, 30);
+        textureAtlas.addRegion(baseName + "1_2_DOWN", sourceImage, 150, 150, 30, 30);
+        textureAtlas.addRegion(baseName + "1_3_DOWN", sourceImage, 200, 200, 30, 30);
+        textureAtlas.addRegion(baseName + "1_4_DOWN", sourceImage, 250, 250, 30, 30);
+        textureAtlas.addRegion(baseName + "1_5_DOWN", sourceImage, 300, 300, 30, 30);
+        textureAtlas.addRegion(baseName + "1_6_DOWN", sourceImage, 350, 350, 30, 30);
+        textureAtlas.addRegion(baseName + "1_7_DOWN", sourceImage, 400, 400, 30, 30);
     }
 
     public TextureRegion getScreenTexture(Class<?> tagClass, AtlasCoordinates coords) {
-        String regionName = String.format(regionNameTemplateLookup.get(tagClass), coords.getLevel(), coords.getColumn());
+        String regionName = String.format(regionNameTemplateLookup.get(tagClass),
+                                          coords.getLevel(), coords.getColumn(), coords.getUpDown());
         return textureAtlas.findRegion(regionName);
     }
 
@@ -72,5 +73,10 @@ public class GameScreenAtlas {
     public static class AtlasCoordinates {
         private int level;
         private int column;
+        private UpDown upDown;
+    }
+
+    public enum UpDown {
+        UP, DOWN
     }
 }
