2f8d2aceba7e2f2302b33dd9fff110435e1717fa
ferminho <ferminho@gmail.com>
2017-07-30 17:02:36 +0200

    Setting default res in desktop mode

--- desktop/src/com/alienshots/ludum/desktop/DesktopLauncher.java
+++ desktop/src/com/alienshots/ludum/desktop/DesktopLauncher.java
@@ -7,6 +7,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.setFromDisplayMode(LwjglApplicationConfiguration.getDesktopDisplayMode());
+		config.fullscreen = false;
+		config.width = 1280;
+		config.height = 720;
+		config.resizable = true;
+		config.vSyncEnabled = true;
+
 		new LwjglApplication(new PowerCharge(), config);
 	}
 }
